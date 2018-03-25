5e6ff2e856eec8bb73089dc113bd14cccb5f95ca
Jeff Yokup <jyokup@iastate.edu>
2018-03-25 11:05:55 -0500

    Jeff is a god and fixed.

--- TestCode/DoublyLinkedList-Student1.java
+++ TestCode/DoublyLinkedList-Student1.java
@@ -27,59 +27,6 @@ class DoublyLinkedList{
 		tail = null;
 	}
 
-	/**
-	 * Insert an element at the head
-	 * 
-	 * @param x Element to be inserted
-	 */
-	public void insertHead(int x){
-		Link newLink = new Link(x); //Create a new link with a value attached to it
-		if(isEmpty()) //Set the first element added to be the tail
-			tail = newLink;
-		else
-			head.previous = newLink; // newLink <-- currenthead(head)
-		newLink.next = head; // newLink <--> currenthead(head)
-		head = newLink; // newLink(head) <--> oldhead
-	}
-
-	/**
-	 * Insert an element at the tail
-	 * 
-	 * @param x Element to be inserted
-	 */
-	public void insertTail(int x){
-		Link newLink = new Link(x);
-		newLink.next = null; // currentTail(tail)     newlink -->
-		tail.next = newLink; // currentTail(tail) --> newLink -->
-		newLink.previous = tail; // currentTail(tail) <--> newLink -->
-		tail = newLink; // oldTail <--> newLink(tail) -->
-	}
-
-	/**
-	 * Delete the element at the head
-	 * 
-	 * @return The new head
-	 */
-	public Link deleteHead(){
-		Link temp = head;
-		head = head.next; // oldHead <--> 2ndElement(head)
-		head.previous = null; // oldHead --> 2ndElement(head) nothing pointing at old head so will be removed
-		if(head == null)
-			tail = null;
-		return temp;
-	}
-
-	/**
-	 * Delete the element at the tail
-	 * 
-	 * @return The new tail
-	 */
-	public Link deleteTail(){
-		Link temp = tail;
-		tail = tail.previous; // 2ndLast(tail) <--> oldTail --> null
- 		tail.next = null; // 2ndLast(tail) --> null
-		return temp;
-	}
 
 	/**
 	 * Delete the element from somewhere in the list
@@ -176,6 +123,62 @@ class Link{
 	public Link(int value){
 		this.value = value;
 	}
+
+
+	/**
+	 * Insert an element at the head
+	 *
+	 * @param x Element to be inserted
+	 */
+	public void insertHead(int x){
+		Link newLink = new Link(x); //Create a new link with a value attached to it
+		if(isEmpty()) //Set the first element added to be the tail
+			tail = newLink;
+		else
+			head.previous = newLink; // newLink <-- currenthead(head)
+		newLink.next = head; // newLink <--> currenthead(head)
+		head = newLink; // newLink(head) <--> oldhead
+	}
+
+	/**
+	 * Insert an element at the tail
+	 *
+	 * @param x Element to be inserted
+	 */
+	public void insertTail(int x){
+		Link newLink = new Link(x);
+		newLink.next = null; // currentTail(tail)     newlink -->
+		tail.next = newLink; // currentTail(tail) --> newLink -->
+		newLink.previous = tail; // currentTail(tail) <--> newLink -->
+		tail = newLink; // oldTail <--> newLink(tail) -->
+	}
+
+	/**
+	 * Delete the element at the head
+	 *
+	 * @return The new head
+	 */
+	public Link deleteHead(){
+		Link temp = head;
+		head = head.next; // oldHead <--> 2ndElement(head)
+		head.previous = null; // oldHead --> 2ndElement(head) nothing pointing at old head so will be removed
+		if(head == null)
+			tail = null;
+		return temp;
+	}
+
+	/**
+	 * Delete the element at the tail
+	 *
+	 * @return The new tail
+	 */
+	public Link deleteTail(){
+		Link temp = tail;
+		tail = tail.previous; // 2ndLast(tail) <--> oldTail --> null
+		tail.next = null; // 2ndLast(tail) --> null
+		return temp;
+	}
+
 
 	/**
 	 * Displays the node
